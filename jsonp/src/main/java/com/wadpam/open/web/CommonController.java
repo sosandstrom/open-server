1d576e213e3cb2aa1e397edc187ec449dc2447ab
Mattias Levin <mattias.levin@gmail.com>
2012-12-12 19:52:04 +0700

    The uuid generate now can be configured with a callback method that gets called when an id is generated.

--- jsonp/src/main/java/com/wadpam/open/web/CommonController.java
+++ jsonp/src/main/java/com/wadpam/open/web/CommonController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,8 +24,15 @@ import java.util.UUID;
 public class CommonController extends AbstractRestController {
     private static final Logger LOG = LoggerFactory.getLogger(CommonController.class);
 
+
+    private UUIDCreatedListener uuidCallback;
+
     /**
      * Get a cross platform unique id.
+     * @param appArg1 an option parameter the device can set that will be included in
+     *                the callback to any registered callback listener.
+     * @param appArg2 an option parameter the device can set that will be included in
+     *                the callback to any registered callback listener.
      * @return a UUID type 4
      */
     @RestReturn(value=Map.class, entity=Map.class, code={
@@ -32,14 +40,31 @@ public class CommonController extends AbstractRestController {
     })
     @RequestMapping(value="uuid", method= RequestMethod.GET)
     @ResponseBody
-    public Map<String, String> generateUUID4(HttpServletRequest request) {
+    public Map<String, String> generateUUID4(HttpServletRequest request,
+                                             @RequestParam String appArg1,
+                                             @RequestParam String appArg2) {
         LOG.debug("Generate UUID");
 
         // Generate a type 4 UUID
         UUID uuid = UUID.randomUUID();
 
+        // If we have a callback method set, call it
+        if (null != uuidCallback) {
+            uuidCallback.uuidCreated(uuid.toString(), appArg1, appArg2);
+        }
+
         Map<String, String> response = new HashMap<String, String>();
         response.put("uuid", uuid.toString());
         return response;
+    }
+
+
+    // Setters and getters
+    public UUIDCreatedListener getUuidCallback() {
+        return uuidCallback;
+    }
+
+    public void setUuidCallback(UUIDCreatedListener uuidCallback) {
+        this.uuidCallback = uuidCallback;
     }
 }
