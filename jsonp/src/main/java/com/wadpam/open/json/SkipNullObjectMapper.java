6e224047288cd352956a5d31e25570ad398eb9c6
sophea <sm@goldengekko.com>
2013-04-22 18:15:20 +0700

    add FAIL_ON_UNKNOWN_PROPERTIES,false

--- jsonp/src/main/java/com/wadpam/open/json/SkipNullObjectMapper.java
+++ jsonp/src/main/java/com/wadpam/open/json/SkipNullObjectMapper.java
@@ -1,5 +1,6 @@
 package com.wadpam.open.json;
 
+import org.codehaus.jackson.map.DeserializationConfig.Feature;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 
@@ -10,5 +11,6 @@ public class SkipNullObjectMapper extends ObjectMapper {
 
     public void init() {
         getSerializationConfig().setSerializationInclusion(Inclusion.NON_NULL);
+        getDeserializationConfig().set(Feature.FAIL_ON_UNKNOWN_PROPERTIES,false);
     }
 }
